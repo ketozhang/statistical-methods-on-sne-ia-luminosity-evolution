d20abfe9d27543986ca01db1d4bee2b268bc0907
Keto Zhang <keto.zhang@gmail.com>
2020-09-18 13:48:41 -0700

    Init script for savio

--- dataloader.py
+++ dataloader.py
@@ -11,10 +11,11 @@ DATAPATH = Path("./data")
 RESULTSPATH = Path("./results")
 FIGURESPATH = Path("./figures")
 
+
 def load_age_sample_from_mcmc_chains(dataset, mode="read", **kwargs):
     """Return a random sample of the mcmc chains dataset for all SNe available.
 
-    dataset : str {"campbell", "campbellG", "gupta"} 
+    dataset : str {"campbell", "campbellG", "gupta"}
         Dataset to load.
     mode : str {"read", "write"}
         If write, redo the creation of the mcmc chain sample and also write to file.
@@ -23,15 +24,17 @@ def load_age_sample_from_mcmc_chains(dataset, mode="read", **kwargs):
     **kwargs : keyword arguments
         Used when mode="write" and passed onto the function `create_age_sample_from_mcmc_chains`.
     """
-    dataset_path = DATAPATH/"mcmc_chains"/dataset
-    assert dataset_path.exists(), f"{dataset_path} does not exists."
-    
+    dataset_path = DATAPATH / "mcmc_chains" / dataset
+
     if mode == "write":
         return _create_age_sample_from_mcmc_chains(dataset, dataset_path, **kwargs)
     if mode == "read":
-        return pd.read_table(DATAPATH/f"{dataset}_samples.tsv")
-    
-def _create_age_sample_from_mcmc_chains(dataset, dataset_path, sample_size=10000, random_state=RNG):
+        return pd.read_table(DATAPATH / f"{dataset}_samples.tsv")
+
+
+def _create_age_sample_from_mcmc_chains(
+    dataset, dataset_path, sample_size=10000, random_state=RNG
+):
     dfs = []
     all_dataset_files = list(dataset_path.glob("*.tsv"))
     for i, sn_chain_path in enumerate(all_dataset_files):
@@ -40,29 +43,36 @@ def _create_age_sample_from_mcmc_chains(dataset, dataset_path, sample_size=10000
         # Header takes up 2 rows
         nheaders = 2
         nrows = sum(1 for line in open(sn_chain_path)) - nheaders
-        skiprows = [1] + sorted(random_state.choice(range(nheaders, nrows+nheaders), nrows-sample_size, replace=False))
+        skiprows = [1] + sorted(
+            random_state.choice(
+                range(nheaders, nrows + nheaders), nrows - sample_size, replace=False
+            )
+        )
 
-        _df = pd.read_table(sn_chain_path, skiprows=skiprows, usecols=[7], index_col=False)
-        
+        _df = pd.read_table(
+            sn_chain_path, skiprows=skiprows, usecols=[7], index_col=False
+        )
+
         # Set the index as the SNID parsed from its filename
         snid = re.findall("SN(\d+)_", sn_chain_path.name)[0]
-        _df['snid'] = [snid]*len(_df)
+        _df["snid"] = [snid] * len(_df)
         dfs.append(_df)
     print(f"{len(all_dataset_files)}/{len(all_dataset_files)}", end="\r")
-        
+
     df = pd.concat(dfs)[["snid", "age"]]
-    df.to_csv(DATAPATH/f"{dataset}_samples.tsv", sep="\t", index=False)
-    return 
+    df.to_csv(DATAPATH / f"{dataset}_samples.tsv", sep="\t", index=False)
+    return
+
 
 def load_hr(dataset):
     """
-    dataset : str {"campbell", "campbellG", "gupta"} 
+    dataset : str {"campbell", "campbellG", "gupta"}
         Dataset to load.
     """
-    dataset_path = DATAPATH/"campbell_local_r19t1.txt"
-    assert dataset_path.exists(), f"{dataset_path} does not exists."
-    
+    dataset_path = DATAPATH / "campbell_local_r19t1.txt"
+
     return ascii.read(dataset_path).to_pandas()
+
 
 def clean_data(age_df, hr_df):
     age_df = age_df.set_index("snid").sort_index()
@@ -70,23 +80,24 @@ def clean_data(age_df, hr_df):
     hr_df = load_hr("campbell")
     hr_df = hr_df
     hr_df.columns = hr_df.columns.str.lower()
-    hr_df = (hr_df.rename(columns={
-        "sdss": "snid",
-        "e_hr": "hr_err"
-    })
-             .set_index("snid")
-             .sort_index()[["hr", "hr_err"]])
+    hr_df = (
+        hr_df.rename(columns={"sdss": "snid", "e_hr": "hr_err"})
+        .set_index("snid")
+        .sort_index()[["hr", "hr_err"]]
+    )
 
     in_age_not_hr = set(age_df.index) - set(hr_df.index)
     in_hr_not_age = set(hr_df.index) - set(age_df.index)
     print("Missing from R19 Table 1 of SNID:", in_age_not_hr)
     print("Missing from Campbell MCMC chains of SNID:", in_hr_not_age)
 
-    print("Resulting data will be an inner join of the two remove all SNID mentioned above")
+    print(
+        "Resulting data will be an inner join of the two remove all SNID mentioned above"
+    )
     age_df = age_df.drop(index=in_age_not_hr)
     hr_df = hr_df.drop(index=in_hr_not_age)
     age_snids = age_df.index.unique().tolist()
     hr_snids = age_df.index.unique().tolist()
-    
+
     assert set(age_snids) == set(hr_snids)
     return age_df, hr_df
---
+++ mcmc_models.py
@@ -0,0 +1,56 @@
+import sys
+import numpy
+import pandas as pd
+from dataloader import *
+from linmix import LinMix
+
+np.random.seed(20200918)
+
+
+def run_linmix(age, age_err, hr, hr_err, linmix_kwargs={}, run_kwargs={}):
+    """Estimated mock of Lee20 result."""
+
+    lm = LinMix(x=age, y=hr, xsig=age_err, ysig=hr_err, **linmix_kwargs)
+    lm.run_mcmc(silent=(not VERBOSE), **run_kwargs)
+
+    results = pd.DataFrame({"slope": lm.chain["alpha"], "intercept": lm.chain["beta"]})
+    return results
+
+
+if __name__ == "__main__":
+    VERBOSE = "-v" in sys.argv[1:]
+
+    # With summary statistics
+    # data = pd.read_csv("data/HRvsAge_Median+STD+Bounds.csv")
+    # age = data["Age_global"]
+    # age_err = data["Age_global_err"]
+    # hr = data["HR"]
+    # hr_err = data["HR_err"]
+    # results = run_linmix(age, age_err, hr, hr_err, linmix_kwargs=dict(K=2))
+    # results.to_csv("results/linmix_1.csv", index=False)
+
+    # # With real data
+    # age_df = load_age_sample_from_mcmc_chains("campbell", mode="read")
+    # hr_df = load_hr("campbell")
+    # age_df, hr_df = clean_data(age_df, hr_df)
+
+    # age = age_df["age"].groupby("snid").mean()
+    # age_err = age_df["age"].groupby("snid").std()
+    # hr = hr_df["hr"]
+    # hr_err = hr_df["hr_err"]
+    # results = run_linmix(age, age_err, hr, hr_err)
+    # results.to_csv("results/linmix_2.csv", index=False)
+
+    # With real data but use the whole Age distribution
+    # Assume each age datapoint is a delta function with zero variance
+    age_df = load_age_sample_from_mcmc_chains("campbell", mode="read")
+    hr_df = load_hr("campbell")
+    age_df, hr_df = clean_data(age_df, hr_df)
+
+    df = age_df.merge(hr_df, on="snid", how="left")
+    age = df["age"]
+    age_err = np.zeros(len(age))
+    hr = df["hr"]
+    hr_err = df["hr_err"]
+    results = run_linmix(age, age_err, hr, hr_err, linmix_kwargs=dict(K=1))
+    results.to_csv("results/linmix_3.csv", index=False)
