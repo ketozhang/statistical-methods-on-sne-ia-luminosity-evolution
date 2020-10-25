ba50b1e1c99b63105b5ad740d9b476bb2689bd8e
Keto Zhang <keto.zhang@gmail.com>
2020-10-24 18:24:24 -0700

    Add argparse to run_model.py

--- run_model.py
+++ run_model.py
@@ -1,18 +1,29 @@
 import numpy as np
+import argparse
 from tqdm import tqdm
 
 from data.dataloader import (clean_data, load_age_sample_from_mcmc_chains,
                              load_hr)
 from models.pymc3_models import LinearModel
 
+DATASETS = {
+    "local": "campbell",
+    "global": "campbellG"
+}
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", choices=["local", "global"])
+    args = parser.parse_args()
+
     np.random.seed(1032020)
-    age_df = load_age_sample_from_mcmc_chains("campbell", mode="read")
-    hr_df = load_hr("campbell")
+    age_df = load_age_sample_from_mcmc_chains(
+        DATASETS[args.dataset], mode="read")
+    hr_df = load_hr(DATASETS[args.dataset])
     age_df, hr_df = clean_data(age_df, hr_df)
     age_matrix = np.array(age_df.groupby("snid").agg(list)["age"].tolist())
 
-    lm = LinearModel('local')
+    lm = LinearModel(args.dataset)
     trace, model = lm.fit(
         hr_df["hr"],
         hr_df["hr_err"],
