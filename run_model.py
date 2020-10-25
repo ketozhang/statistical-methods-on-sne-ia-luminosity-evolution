222e28734cebfc16ba819d4d1c54adf131ee867a
Keto Zhang <keto.zhang@gmail.com>
2020-10-25 15:56:31 -0700

    Fix ValueError on run_model

--- run_model.py
+++ run_model.py
@@ -16,21 +16,22 @@ DATASETS = {
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset", choices=["local", "global"])
-    args = parser.parse_args()
+    args = parser.parse_args(["local"])
 
     np.random.seed(1032020)
     age_df = load_age_sample_from_mcmc_chains(
         DATASETS[args.dataset], mode="read")
-    hr_df = load_hr(DATASETS[args.dataset])
+    hr_df = load_hr()
     age_df, hr_df = clean_data(age_df, hr_df)
+    snids = list(age_df.groupby("snid").groups.keys())
     age_matrix = np.array(age_df.groupby("snid").agg(list)["age"].tolist())
 
-    # GMM
+    # GMM fit on Age
     gmm = GaussianMixture(name=args.dataset)
     gmm.fit(age_matrix, n_components=3)
     gmm_params = gmm.get_params()
     gmm_params.to_csv(gmm.params_fpath)
-    results = gmm.get_results()
+    results = {snid: result for snid, result in zip(snids, gmm.get_results())}
     with gmm.results_fpath.open("wb") as f:
         pickle.dump(results, f)
 
